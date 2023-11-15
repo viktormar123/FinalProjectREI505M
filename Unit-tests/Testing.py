e4e9ebdea577e7fd0cb8c70652f05adee6e173ae
viktormar123 <134391247+viktormar123@users.noreply.github.com>
2023-11-15 23:46:09 +0000

    Update Testing.py

--- Unit-tests/Testing.py
+++ Unit-tests/Testing.py
@@ -1,10 +1,10 @@
 import os
 
 if __name__ == '__main__':
-    Connect4Game = 'Connect-4_Game_test'
-    Connect4Win = "Running/Objects/Tests/GameClass/game_win_test.py"
-    Connect4Draw = "Running/Objects/Tests/GameClass/game_draw_test.py"
-    QLearningAgent = 'Running/Objects/Classes/QLearningAgentClass.py'
+    Connect4Game = 'Connect-4_test'
+    Connect4Win = "Win_test.py"
+    Connect4Draw = "Draw_test.py"
+    QLearningAgent = 'Bots/Q-Learning_Agent.py'
 
 
     os.system(f'pytest {Connect4Game}.py')
